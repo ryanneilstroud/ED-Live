af1d49642f670a97213a8e131e8ee1e1cfd66f23
Ryan Neil Stroud <ryanstroud75@gmail.com>
2023-09-16 18:26:35 +0800

    Initial Commit

---
+++ LiveMentoringSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveMentoringSession
+//
+//  Created by Ryan Neil Stroud on 16/9/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
